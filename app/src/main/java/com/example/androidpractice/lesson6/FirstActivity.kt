fa1dc5f37fd27549822992f1898456e5f6647a70
tnjobaya <tnjobaya@kinghan.com>
2022-02-18 15:41:37 +0800

    0218. lesson6: example of launchMode: singleTop.

---
+++ app/src/main/java/com/example/androidpractice/lesson6/FirstActivity.kt
@@ -0,0 +1,22 @@
+package com.example.androidpractice.lesson6
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import com.example.androidpractice.R
+
+class FirstActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first_6)
+    }
+
+    public fun gotoFirst(view: View) {
+        startActivity(Intent(this, FirstActivity::class.java))
+    }
+
+    public fun gotoSecond(view: View) {
+        startActivity(Intent(this, SecondActivity::class.java))
+    }
+}
