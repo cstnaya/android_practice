29a1b98aa06b0e862bf1fc3fc7732b929be3601d
tnjobaya <tnjobaya@kinghan.com>
2022-03-09 15:16:13 +0800

    0309. lesson13: design login UI.

---
+++ app/src/main/java/com/example/androidpractice/lesson13/LoginUIActivity.kt
@@ -0,0 +1,13 @@
+package com.example.androidpractice.lesson13
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.androidpractice.R
+
+class LoginUIActivity: Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_loginui)
+
+    }
+}
